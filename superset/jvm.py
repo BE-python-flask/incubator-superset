e0c31ca7fa7cd1e60b4ec068fc02e9c360842c78
jiajie <jiajie.zhang@transwarp.io>
2018-10-11 10:47:57 +0800

    WARP-25634, There exists conflict between dnw 1.0.7 and guardian client 3.0, so we load license jar first

--- superset/jvm.py
+++ superset/jvm.py
@@ -6,8 +6,8 @@ from jpype import *
 JAR_PATH = '/usr/local/lib'
 JAR_SUFFIX = 'transwarp-6.0.0.jar'
 GUARDIAN_SITE_PATH = '/etc/pilot/conf/'
-# LICENSE_JAR = 'pilot-license-1.0-transwarp-6.0.0.jar'
-# GAURDIAN_JAR = 'guardian-client-2.0-transwarp-6.0.0.jar'
+LICENSE_JAR = 'pilot-license-1.0-transwarp-6.0.0.jar'
+GAURDIAN_JAR = 'guardian-client-3.0-transwarp-6.0.0.jar'
 
 
 def start_jvm():
@@ -26,7 +26,11 @@ def shutdown_jvm():
 
 def get_java_paths():
     paths = [GUARDIAN_SITE_PATH, ]
-    os.chdir(JAR_PATH)
-    for file in glob.glob("*{}".format(JAR_SUFFIX)):
-        paths.append('{}/{}'.format(JAR_PATH, file))
+    # os.chdir(JAR_PATH)
+    # for file in glob.glob("*{}".format(JAR_SUFFIX)):
+    #     paths.append('{}/{}'.format(JAR_PATH, file))
+    # There exists conflict between dnw 1.0.7 and guardian client 3.0,
+    # so here we load license jar first.
+    paths.append('{}/{}'.format(JAR_PATH, LICENSE_JAR))
+    paths.append('{}/{}'.format(JAR_PATH, GAURDIAN_JAR))
     return paths
